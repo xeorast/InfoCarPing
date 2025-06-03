3e3c5b7d612bca755aab92034ffb78309f195b92
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2025-06-03 18:44:52 +0200

    Add config file check

--- InfoCarPing/Program.cs
+++ InfoCarPing/Program.cs
@@ -3,109 +3,118 @@ using System.Text.Json;
 
 internal class Program
 {
-	private static async Task Main()
-	{
-		Console.WriteLine( "CarInfoPing by xeorast" );
-		Console.WriteLine();
+    private static async Task Main()
+    {
+        Console.WriteLine("CarInfoPing by xeorast");
+        Console.WriteLine();
 
-		var config = File.ReadAllText( "config.json" );
-		var options = JsonSerializer.Deserialize<InfoCarClientApiOptions>( config )!;
+        if (!File.Exists("config.json"))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"brak pliku ""config.json""");
+            Console.ResetColor();
+            Console.ReadKey();
+            return;
+        }
 
-		InfoCarApiClient infoCar = new( options );
+        var config = File.ReadAllText("config.json");
+        var options = JsonSerializer.Deserialize<InfoCarClientApiOptions>(config)!;
 
-		try
-		{
-			await infoCar.LogIn();
-		}
-		catch ( Exception e )
-		{
-			PrintException( e );
-			Beep( 3 );
-			Console.ReadKey();
-			return;
-		}
+        InfoCarApiClient infoCar = new(options);
 
-		while ( true )
-		{
-			ExamDayDto[] days;
-			try
-			{
-				days = await infoCar.GetExams( options.Category, 42, options.MaxWaitingTime );
-			}
-			catch ( TransientPingException e )
-			{
-				Console.ForegroundColor = ConsoleColor.Yellow;
-				Console.WriteLine( e.Message + Environment.NewLine );
-				Console.ResetColor();
-				await Task.Delay( Randomize( 30 * 1000, 10 * 1000 ) );
-				continue;
-			}
-			catch ( Exception e )
-			{
-				PrintException( e );
-				Beep( 3 );
-				await Task.Delay( Randomize( 30 * 1000, 10 * 1000 ) );
-				continue;
-			}
+        try
+        {
+            await infoCar.LogIn();
+        }
+        catch (Exception e)
+        {
+            PrintException(e);
+            Beep(3);
+            Console.ReadKey();
+            return;
+        }
 
-			Console.WriteLine( $"Status z {DateTime.Now:g}:" );
+        while (true)
+        {
+            ExamDayDto[] days;
+            try
+            {
+                days = await infoCar.GetExams(options.Category, 42, options.MaxWaitingTime);
+            }
+            catch (TransientPingException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(e.Message + Environment.NewLine);
+                Console.ResetColor();
+                await Task.Delay(Randomize(30 * 1000, 10 * 1000));
+                continue;
+            }
+            catch (Exception e)
+            {
+                PrintException(e);
+                Beep(3);
+                await Task.Delay(Randomize(30 * 1000, 10 * 1000));
+                continue;
+            }
 
-			PrintExams( days );
+            Console.WriteLine($"Status z {DateTime.Now:g}:");
 
-			if ( days.Any() )
-			{
-				Beep( 4 );
-			}
+            PrintExams(days);
 
-			Console.WriteLine();
+            if (days.Any())
+            {
+                Beep(4);
+            }
 
-			await Task.Delay( Randomize( 60 * 1000, 10 * 1000 ) );
-		}
-	}
+            Console.WriteLine();
 
-	static void PrintException( Exception e )
-	{
-		Console.BackgroundColor = ConsoleColor.Red;
-		Console.ForegroundColor = ConsoleColor.White;
-		Console.WriteLine( e is InfoCarPingException ? e.Message : e );
-		Console.WriteLine();
-		Console.ResetColor();
-	}
+            await Task.Delay(Randomize(60 * 1000, 10 * 1000));
+        }
+    }
 
-	static void PrintExams( ExamDayDto[] examDays )
-	{
-		Console.BackgroundColor = ConsoleColor.Green;
-		Console.ForegroundColor = ConsoleColor.Black;
+    static void PrintException(Exception e)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(e is InfoCarPingException ? e.Message : e);
+        Console.WriteLine();
+        Console.ResetColor();
+    }
 
-		foreach ( var day in examDays )
-		{
-			Console.WriteLine( $"{day.Day}:" );
+    static void PrintExams(ExamDayDto[] examDays)
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Black;
 
-			foreach ( var exam in day.Exams )
-			{
-				Console.WriteLine( $"  {exam.Time}: {exam.Places} miejsc" );
-			}
-		}
+        foreach (var day in examDays)
+        {
+            Console.WriteLine($"{day.Day}:");
 
-		Console.ResetColor();
+            foreach (var exam in day.Exams)
+            {
+                Console.WriteLine($"  {exam.Time}: {exam.Places} miejsc");
+            }
+        }
 
-		if ( examDays.Length == 0 )
-		{
-			Console.WriteLine( "  Brak wolnych miejsc." );
-		}
-	}
+        Console.ResetColor();
 
-	static void Beep( int times )
-	{
-		for ( int i = 0; i < times; i++ )
-		{
-			Console.Beep();
-		}
-	}
+        if (examDays.Length == 0)
+        {
+            Console.WriteLine("  Brak wolnych miejsc.");
+        }
+    }
 
-	static int Randomize( int middle, int maxOffset )
-	{
-		var multiplier = Random.Shared.NextDouble() * 2 - 1; // -1 to 1
-		return middle + (int)( maxOffset * multiplier );
-	}
+    static void Beep(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            Console.Beep();
+        }
+    }
+
+    static int Randomize(int middle, int maxOffset)
+    {
+        var multiplier = Random.Shared.NextDouble() * 2 - 1; // -1 to 1
+        return middle + (int)( maxOffset * multiplier );
+    }
 }
